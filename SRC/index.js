8d729657dba76c62a0220f58082613fae01f6299
pyh198612 <76249346+pyh198612@users.noreply.github.com>
2021-02-13 15:50:45 +0100

    First day forecast completed

--- SRC/index.js
+++ SRC/index.js
@@ -1,4 +1,7 @@
 //Main session time
+
+let apiKey = "3cbad6f9a349042eb44901a3bdcb3200";
+
 function showCurrentDate (timestamp) {
   let time = new Date(timestamp);
   //console.log(time);
@@ -6,7 +9,8 @@ function showCurrentDate (timestamp) {
   let conDate = ("0"+date).slice(-2);
   let month = ("0"+(time.getMonth()+1)).slice(-2);
   let year = time.getFullYear();
-  return `${conDate}.${month}.${year} Today`;
+  return `${conDate}.${month}.${year}`;
+  //return `${formatDate(timestamp)} Today`;
 }
 
 function showCurrentDay (timestamp){
@@ -46,19 +50,15 @@ console.log(response.data);
   let nowTemp = document.querySelector ("#current-temp");
   nowTemp.innerHTML = temperature; 
   
-  let maxTemperature = Math.round (response.data.main.temp_max);
-  let nowMaxTemp = document.querySelector ("#now-max-temp");
-  nowMaxTemp.innerHTML = maxTemperature; 
-  
-  let minTemperature = Math.round (response.data.main.temp_min);
-  let nowMinTemp = document.querySelector ("#now-min-temp");
-  nowMinTemp.innerHTML = minTemperature; 
-
   let description = response.data.weather[0].description; 
   let nowDescription = document.querySelector ("#now-weather-description");
   nowDescription.innerHTML = description; 
-  
 
+  let windSpeed = Math.round(response.data.wind.speed);
+  let nowWindSpeed = document.querySelector ("#wind-speed");
+  nowWindSpeed.innerHTML = windSpeed;
+  
+  
   let timestamp = response.data.dt;
   //console.log(timestamp);
   let timezone = response.data.timezone;
@@ -115,19 +115,118 @@ console.log(response.data);
 
 function showCityPosition (response) {
   function showCityPrecipitation (response) {
+    console.log (response.data);
     let cityPrecipitation = Math.round ((response.data.daily[0].pop)*100);
     let cityPrecipitationPercentage = document.querySelector ("#now-precipitation");
     cityPrecipitationPercentage.innerHTML = cityPrecipitation;
   }
+  
+  function showMinMaxTemp (response){
+    let maxTemperature = Math.round (response.data.daily[0].temp.max);
+    let minTemperature = Math.round (response.data.daily[0].temp.min);
+    let nowMaxTemp = document.querySelector ("#now-max-temp");
+    let nowMinTemp = document.querySelector ("#now-min-temp");
+    nowMaxTemp.innerHTML = maxTemperature; 
+    nowMinTemp.innerHTML = minTemperature; 
+  }
+  
+  function forecastDate (timestamp) {
+    let forecastTime = new Date (timestamp);
+    //let forecastDate = ("0"+forecastTime.getDate()).slice(-2);
+    //let forecastMonth = ("0"+(forecastTime.getMonth()+1)).slice(-2);
+    //let forecastYear = forecastTime.getFullYear(); 
+    //return `${forecastDate}.${forecastMonth}.${forecastYear}`;
+    return `${showCurrentDate(timestamp)}`;
+  }
 
+  function forecastWeekday (timestamp) {
+    let forecastTime = new Date (timestamp);
+    //let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    //let forecastDay = days [forecastTime.getDay()]; 
+    return `${showCurrentDay(timestamp)}`;
+  }
+
+  function showForecast (response){
+    let forecastElement = document.querySelector ("#forecast");
+    let forecast = response.data.daily[1];
+    console.log (forecast);
+
+    let iconCode = forecast.weather[0].icon;
+      if (iconCode === "02d") {
+      forecastSrc = `IMG/cloudy with sun_02d.png`; 
+      forecastAlt = forecast.weather[0].description;
+    } else if (iconCode === "02n") {
+      forecastSrc = `IMG/few clouds_02n.png`;
+      forecastAlt =forecast.weather[0].description;
+    } else if (iconCode === "01d") {
+      forecastSrc = `IMG/sunny_01d.png`; 
+      forecastAlt =forecast.weather[0].description;
+    } else if (iconCode === "01n") {
+      forecastSrc = `IMG/clear sky_01n.png`; 
+      forecastAlt =forecast.weather[0].description;
+    } else if (iconCode === "03d" || iconCode === "03n"){
+      forecastSrc = `IMG/scattered clouds_03.png`; 
+      forecastAlt = forecast.weather[0].description;
+    } else if (iconCode === "04d" || iconCode === "04n"){
+      forecastSrc = `IMG/broken clouds_04.png`; 
+      forecastAlt = forecast.weather[0].description;    
+    } else if (iconCode === "09d" || iconCode === "09n"){
+      forecastSrc = `IMG/shower rain_09.png`; 
+      forecastAlt = forecast.weather[0].description;   
+    } else if (iconCode === "10d"){ 
+      forecastSrc = `IMG/rain_10d.png`; 
+      forecastAlt = forecast.weather[0].description;
+    } else if (iconCode === "10n"){ 
+      forecastSrc = `IMG/rain_10n.png`; 
+      forecastAlt = forecast.weather[0].description;
+    } else if (iconCode === "11d" || iconCode === "11n"){
+      forecastSrc = `IMG/thunderstorm_11.png`; 
+      forecastAlt = forecast.weather[0].description;   
+    } else if (iconCode === "13d" || iconCode === "13n"){
+      forecastSrc = `IMG/snow_13.png`; 
+      forecastAlt = forecast.weather[0].description;   
+    } else if (iconCode === "50d" || iconCode === "50n"){
+      forecastSrc = `IMG/mist_50.png`; 
+      forecastAlt = forecast.weather[0].description;   
+    }
+  
+    forecastElement.innerHTML = `
+    <p>
+      <div class="row align-items-center">
+        <div class="col-1"></div>
+        <div class="col-2">
+          ${forecastDate ((forecast.dt)*1000)}
+        </div>
+        <div class="col-2">
+          ${forecastWeekday ((forecast.dt)*1000)}
+        </div>
+        <div class="col-2">
+          <img src="${forecastSrc}" alt="${forecastAlt}" class="small-image" id="forecast-icon"/>
+        </div>
+        <div class="col-2">
+          ${Math.round(forecast.temp.min)}° / ${Math.round(forecast.temp.max)} °C
+        </div>
+        <div class="col-2">
+          <i class="fas fa-umbrella"></i> ${(forecast.pop)*100}% <br/>
+          <i class="fas fa-wind"></i> ${Math.round(forecast.wind_speed)} m/s
+        </div>
+        <div class="col-1"></div>
+      </div>
+    <p>
+    <hr />
+    `;
+  } 
+
+  
   let cityLon = response.data[0].lon;
   let cityLat = response.data[0].lat;
-  let apiKey = "3cbad6f9a349042eb44901a3bdcb3200";
   let units = "metric";
   let excludes = "current,hourly,minutely,alerts";
-  let apiCityPrecipitationUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=${excludes}&appid=${apiKey}&units=${units}`;
+  let apiOneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=${excludes}&appid=${apiKey}&units=${units}`;
 
-  axios.get(apiCityPrecipitationUrl).then(showCityPrecipitation);
+  axios.get(apiOneCallUrl).then(showCityPrecipitation);
+  axios.get(apiOneCallUrl).then(showMinMaxTemp);
+  axios.get(apiOneCallUrl).then (showForecast);
 }
 
 function searchCity (event) {
@@ -136,7 +235,6 @@ function searchCity (event) {
   let h1= document.querySelector ("h1");
   h1.innerHTML = `${cityInput.value}`;
   
-  let apiKey = "3cbad6f9a349042eb44901a3bdcb3200";
   let city = cityInput.value; 
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
@@ -144,6 +242,7 @@ function searchCity (event) {
 
   let apiGeocodingUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityInput.value}&appid=${apiKey}`;
   axios.get(apiGeocodingUrl).then (showCityPosition);
+
 }
 
 
@@ -167,7 +266,6 @@ function handleCurrentButton (event) {
   function showPosition (position) {
     let latitude = position.coords.latitude; 
     let longitude = position. coords.longitude;
-    let apiKey = "3cbad6f9a349042eb44901a3bdcb3200";
     let units = "metric";
     let excludes = "current,hourly,minutely,alerts";
     let apiLocationUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
@@ -183,3 +281,7 @@ navigator.geolocation.getCurrentPosition (showPosition);
 
 let currentButton = document.querySelector (".current-button");
 currentButton.addEventListener ("click", handleCurrentButton);
+
+
+
+
